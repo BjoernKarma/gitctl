5ef84e9e07f10afbfba5194fdd295d8f8482f48c
Bjoern Kottner <1780890+BjoernKarma@users.noreply.github.com>
2025-02-25 15:13:38 +0100

    feat: Extract commands

--- app/cmd/root.go
+++ app/cmd/root.go
@@ -1,9 +1,10 @@
 package cmd
 
 import (
-	"ethical-developer/cli/gitctl/gitrepo"
+	"ethical-developer/cli/gitctl/config"
 	"log"
-	"os"
+
+	"github.com/pkg/errors"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -11,34 +12,25 @@ import (
 
 var (
 	// Used for flags.
-	cfgFile string
-	Verbose bool
+	configFile  string
+	quiet       bool
+	verbose     bool
+	debug       bool
+	local       bool
+	dryRun      bool
+	color       bool
+	concurrency string
+	baseDirs    []string
+)
 
-	rootCmd = &cobra.Command{
-		Use:   "gitctl",
-		Short: "Run git commands on multiple git repositories",
-		Long: `Run git commands on multiple git repositories. 
+var rootCmd = &cobra.Command{
+	Use:   "gitctl",
+	Short: "Run git commands on multiple git repositories",
+	Long: `Run git commands on multiple git repositories. 
 For example, you can run 'gitctl pull' to pull all the git 
 repositories in the base directories.`,
-		Version: "0.1.0",
-	}
-
-	statusCmd = &cobra.Command{
-		Use:   "status",
-		Short: "Execute git status on multiple git repositories.",
-		Run: func(cmd *cobra.Command, args []string) {
-			gitrepo.RunGitCommand(gitrepo.GitStatus, viper.GetStringSlice("base.dir"))
-		},
-	}
-
-	pullCmd = &cobra.Command{
-		Use:   "pull",
-		Short: "Execute git pull on multiple git repositories.",
-		Run: func(cmd *cobra.Command, args []string) {
-			gitrepo.RunGitCommand(gitrepo.GitPull, viper.GetStringSlice("base.dir"))
-		},
-	}
-)
+	Version: "1.2.0",
+}
 
 // Execute executes the root command.
 func Execute() error {
@@ -46,45 +38,58 @@ func Execute() error {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(InitConfig)
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/gitctl.yaml)")
-	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().StringVar(&configFile, "config", "", "config file (default is $HOME/.config/gitctl.yaml)")
+	rootCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "suppress output")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "debug output")
+	rootCmd.PersistentFlags().BoolVarP(&local, "local", "l", false, "run with working directory used as base directory")
+	rootCmd.PersistentFlags().BoolVarP(&dryRun, "dryRun", "D", false, "run with dry run mode")
+	rootCmd.PersistentFlags().BoolVarP(&color, "color", "c", true, "color output")
+	rootCmd.PersistentFlags().StringVarP(&concurrency, "concurrency", "C", "1", "number of concurrent operations")
+	rootCmd.PersistentFlags().StringSliceVar(&baseDirs, "base.dirs", []string{}, "base directories for git repositories")
+
+	// Bind flags to Viper settings
+	_ = viper.BindPFlag(config.GitCtlQuiet, rootCmd.PersistentFlags().Lookup("quiet"))
+	_ = viper.BindPFlag(config.GitCtlVerbose, rootCmd.PersistentFlags().Lookup("verbose"))
+	_ = viper.BindPFlag(config.GitCtlDebug, rootCmd.PersistentFlags().Lookup("debug"))
+	_ = viper.BindPFlag(config.GitCtlLocal, rootCmd.PersistentFlags().Lookup("local"))
+	_ = viper.BindPFlag(config.GitCtlDryRun, rootCmd.PersistentFlags().Lookup("dryRun"))
+	_ = viper.BindPFlag(config.GitCtlColor, rootCmd.PersistentFlags().Lookup("color"))
+	_ = viper.BindPFlag(config.GitCtlConcurrency, rootCmd.PersistentFlags().Lookup("concurrency"))
+	_ = viper.BindPFlag(config.GitCtlBaseDirs, rootCmd.PersistentFlags().Lookup("base.dirs"))
 
 	rootCmd.AddCommand(statusCmd)
 	rootCmd.AddCommand(pullCmd)
 }
 
-func initConfig() {
-	if cfgFile != "" {
+func InitConfig() {
+	if configFile != "" {
 		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-		log.Println("Using config file from flag:", cfgFile)
+		viper.SetConfigFile(configFile)
 	} else {
-		// Find home directory.
-		var home string
-		home = os.Getenv("HOME")
-		if home == "" {
-			var err error
-			home, err = os.UserHomeDir()
-			cobra.CheckErr(err)
-		}
-
-		// Search config in home directory with name "gitctl.yaml" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
 		viper.SetConfigName("gitctl")
-		log.Println("Using config file from home dir:", home)
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath(config.GitctlWorkingDir())
+		viper.AddConfigPath(config.GitctlConfigDir())
 	}
 
+	// Enable reading from environment variables
 	viper.AutomaticEnv()
 
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil {             // Handle errors reading the config file
-		log.Println("fatal error config file: ", err)
-		return
+	// Read the configuration file
+	if err := viper.ReadInConfig(); err != nil {
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if errors.As(err, &configFileNotFoundError) {
+			log.Println("No configuration file found, using defaults and environment variables")
+		}
+	} else {
+		log.Printf("Using configuration file: %s", viper.ConfigFileUsed())
 	}
-	log.Println("Using config file:", viper.ConfigFileUsed())
-	log.Println("Found base dirs:", viper.GetStringSlice("base.dir"))
+
+	// Optionally, print the configuration settings for debugging
+	if config.IsDebug() {
+		log.Printf("Configuration settings: %v", viper.AllSettings())
+	}
 }
